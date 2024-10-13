095f4b5c01247543ffba2def7f535bc112bf6f66
masum <motiur.rahman@northsouth.edu>
2024-10-14 02:43:28 +0600

    :up: Update: on profile edit nid added

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -25,6 +25,9 @@ class ProfileUpdateRequest extends FormRequest
                 'max:255',
                 Rule::unique(User::class)->ignore($this->user()->id),
             ],
+            'nid' => ['required', 'string', 'max:12',
+                Rule::unique(User::class)->ignore($this->user()->id)],
+
         ];
     }
 }
